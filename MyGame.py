1679a1b7084afa6e2db5450b91bbdb58868d827a
TabuGit <itzkindtabu@gmail.com>
2024-10-14 21:41:06 +0300

    Fixed all the problems teacher found

--- MyGame.py
+++ MyGame.py
@@ -21,85 +21,98 @@ def hit_success(hit: int) -> int:
     return ans
 
 
+def set_max_hp() -> int:
+    print('Choose max hp of the players')
+    max_hp = int(input())
+    return max_hp
+
+
+def turns_counter(move: int) -> int:
+    turn = move % 2
+    return turn
+
+
 def launch_human_vs_human():
-    print('Player 1, please type your name:')
+    print('Players, please type your names:')
     player_1 = input()
-    print('Player 2, please type your name:')
     player_2 = input()
     first, second = rd.sample([player_1, player_2], k=2)
-    print('The game has started\n')
     players = [first.capitalize(), second.capitalize()]
-    move_ct = 0
-    hp = [20, 20]
-    while (hp[0] > 0) and (hp[1] > 0):
-        print(f'{players[move_ct % 2]}, hit the rival (choose: 1 - 9)')
+    player_hp_1 = player_hp_2 = set_max_hp()
+    print('The game has started\n')
+    players_hp = [player_hp_1, player_hp_2]
+    move = 0
+    while (all(hp > 0 for hp in players_hp)):
+        print(f'{players[turns_counter(move)]}, hit the rival (choose: 1 - 9)')
         hit = int(input())
         if hit_success(hit):
-            hp[(move_ct + 1) % 2] -= hit
+            players_hp[turns_counter(move - 1)] -= hit
             print(f'You delivered {hit} damage')
         else:
             print('Miss :(')
-        print(players[0], hp[0])
-        print(players[1], hp[1])
-        move_ct += 1
-    print(f'{players[(move_ct + 1) % 2]} has won!')
+        move += 1
+        print(f'{players_hp[0]} {players[0]}, {players_hp[1]} {players[1]}')
+    print(f'{players[turns_counter(move - 1)]} has won!')
 
 
 def launch_human_vs_robot_rand():
     print('Player, please type your name')
     player_human = input()
-    print('The game has started\n')
     players = [player_human.capitalize(), 'Robot']
-    move_ct = 0
-    hp = [20, 20]
-    while (hp[0] > 0) and (hp[1] > 0):
-        if move_ct % 2 == 0:
-            print(f'{players[0]}, hit the robot (choose: 1 - 9)')
-            hit = int(input())
-            if hit_success(hit):
-                hp[1] -= hit
-                print(f'You delivered {hit} damage')
-            else:
-                print('Miss :(')
+    print('The game has started\n')
+    player_hp = robot_hp = set_max_hp()
+    players_hp = [player_hp, robot_hp]
+    move = 0
+    while (all(hp > 0 for hp in players_hp)):
+        print(f'{players[0]}, hit the robot (choose: 1 - 9)')
+        hit = int(input())
+        if hit_success(hit):
+            players_hp[1] -= hit
+            print(f'You delivered {hit} damage')
         else:
-            hit = rd.randint(1, 9)
-            robot_damage = hit_success(hit) * hit
-            hp[0] -= robot_damage
-            print(f'You got {robot_damage} damage from robot')
-        print(players[0], hp[0])
-        print(players[1], hp[1])
-        move_ct += 1
-    print(f'{players[(move_ct + 1) % 2]} has won!')
+            print('Miss :(')
+        move += 1
+        if players_hp[1] > 0:
+            move += 1
+            robot_damage = hit_success(rd.randint(1, 9))
+            players_hp[0] -= robot_damage
+            print(f'{robot_damage} damage from Robot')
+        else:
+            break
+        print(f'{players_hp[0]} {players[0]}, {players_hp[1]} {players[1]}')
+    print(f'{players[turns_counter(move - 1)]} has won!')
 
 
 def launch_human_vs_robot_strat():
     print('Player, please type your name')
     player_human = input()
-    print('The game has started\n')
     players = [player_human.capitalize(), 'Robot']
-    move_ct = 0
-    hp = [20, 20]
-    while (hp[0] > 0) and (hp[1] > 0):
-        if move_ct % 2 == 0:
-            print(f'{players[0]}, hit the robot (choose: 1 - 9)')
-            hit = int(input())
-            if hit_success(hit):
-                hp[1] -= hit
-                print(f'You delivered {hit} damage')
-            else:
-                print('Miss :(')
+    print('The game has started\n')
+    player_hp = robot_hp = set_max_hp()
+    players_hp = [player_hp, robot_hp]
+    move = 0
+    while (all(hp > 0 for hp in players_hp)):
+        print(f'{players[0]}, hit the robot (choose: 1 - 9)')
+        hit = int(input())
+        if hit_success(hit):
+            players_hp[1] -= hit
+            print(f'You delivered {hit} damage')
         else:
-            if hp[0] <= 5:
-                hit = rd.randint(1, hp[0])
+            print('Miss :(')
+        move += 1
+        if players_hp[1] > 0:
+            move += 1
+            if players_hp[0] <= 5:
+                hit = rd.randint(1, players_hp[0])
             else:
                 hit = rd.randint(3, 5)
-            robot_damage = hit_success(hit) * hit
-            hp[0] -= robot_damage
+            robot_damage = hit_success(hit)
+            players_hp[0] -= robot_damage
             print(f'You got {robot_damage} damage from robot')
-        print(players[0], hp[0])
-        print(players[1], hp[1])
-        move_ct += 1
-    print(f'{players[(move_ct + 1) % 2]} has won!')
+        else:
+            break
+        print(f'{players_hp[0]} {players[0]}, {players_hp[1]} {players[1]}')
+    print(f'{players[turns_counter(move - 1)]} has won!')
 
 
 if __name__ == '__main__':
